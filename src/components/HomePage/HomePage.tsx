eec4faf1f9e88bb206a0a8a40b77b1274ede7c4e
zeromountain <sonyeongsan@gmail.com>
2023-04-21 16:06:22 +0900

    feat: 🎸홈 UI 퍼블리싱

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-import { Box, BoxProps } from '@chakra-ui/react';
+import { CONFIG } from '@config';
+
+import { Box, BoxProps, Text, useDisclosure } from '@chakra-ui/react';
+
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+
+import HomeNavigationBar from './_fragments/HomeNavigationBar';
+import RightFloatList from './_fragments/RightFloatList';
 
 interface HomePageContentProps extends BoxProps {}
 
+const containerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
 function HomePageContent({ ...basisProps }: HomePageContentProps) {
-  return <Box {...basisProps}>Hello World</Box>;
+  const {
+    isOpen: isOpenNavbar,
+    onOpen: onOpenNavbar,
+    onClose: onCloseNavbar,
+  } = useDisclosure();
+
+  const {
+    isOpen: isOpenList,
+    onOpen: onOpenList,
+    onClose: onCloseList,
+  } = useDisclosure();
+
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: CONFIG.GOOGLE_MAP_KEY as string,
+    version: '3.47',
+  });
+
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback() {
+    setMap(null);
+  }, []);
+
+  if (!isLoaded) return <Text>로딩중</Text>;
+
+  return (
+    <Box position="relative" {...basisProps}>
+      <HomeNavigationBar
+        isOpenNavbar={isOpenNavbar}
+        onOpenNavbar={onOpenNavbar}
+        onCloseNavbar={onCloseNavbar}
+      />
+      {/* <Box ref={mapRef} w="100vw" h="100vh" position="fixed" bg="red" /> */}
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+          // options={{ mapId: CONFIG.GOOGLE_MAP_KEY }}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      ) : (
+        <></>
+      )}
+      <RightFloatList
+        isOpenList={isOpenList}
+        onOpenList={onOpenList}
+        onCloseList={onCloseList}
+      />
+    </Box>
+  );
 }
 
 export default HomePageContent;
