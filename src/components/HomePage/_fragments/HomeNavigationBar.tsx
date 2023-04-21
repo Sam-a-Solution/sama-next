32416c5175810ee8bbd96861721d69e44cf38b07
zeromountain <sonyeongsan@gmail.com>
2023-04-21 16:02:38 +0900

    feat: 🎸사이드 네비게이션바 추가

---
+++ src/components/HomePage/_fragments/HomeNavigationBar.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import { Flex, Highlight, Text } from '@chakra-ui/react';
+
+import AlignCenterFlex from '@components/common/@Flex/AlignCenterFlex';
+
+import {
+  AccountIcon,
+  EmergencyIcon,
+  LogoutIcon,
+  MenuIcon,
+  WorkManagementIcon,
+} from 'generated/icons/MyIcons';
+
+interface HomeNavigationBarProps {
+  isOpenNavbar: boolean;
+  onOpenNavbar: () => void;
+  onCloseNavbar: () => void;
+}
+
+function HomeNavigationBar({
+  isOpenNavbar,
+  onCloseNavbar,
+  onOpenNavbar,
+}: HomeNavigationBarProps) {
+  return (
+    <Flex
+      position="fixed"
+      zIndex="sticky"
+      h="100vh"
+      flexDir="column"
+      justifyContent="space-between"
+      p="10px"
+      boxShadow="0 0 10px rgba(26, 26, 26, 0.1)"
+      transition="all ease-in 1s"
+      bg="white"
+    >
+      <Flex flexDir="column" gap="40px">
+        <AlignCenterFlex w="100%">
+          {/* TODO: 사이드 네비게이션 펼치기 */}
+          <Flex alignItems="center" gap="23px">
+            <MenuIcon
+              w="24px"
+              h="24px"
+              cursor="pointer"
+              onClick={isOpenNavbar ? onCloseNavbar : onOpenNavbar}
+            />
+            {isOpenNavbar && (
+              <Text textStyle="TitleSmall" color="black">
+                <Highlight
+                  query="samasolution"
+                  styles={{
+                    color: 'primary.500',
+                  }}
+                >
+                  samasolution 님
+                </Highlight>
+              </Text>
+            )}
+          </Flex>
+        </AlignCenterFlex>
+        <Flex w="100%" flexDir="column" gap="10px">
+          <AlignCenterFlex w="100%">
+            {/* TODO: 작업현황관리 모달 열기 */}
+            <Flex w="100%" gap="8px" alignItems="center">
+              <WorkManagementIcon w="24px" h="24px" cursor="pointer" />
+              {isOpenNavbar && (
+                <Text textStyle="TitleSmall" color="black">
+                  작업 현황 관리
+                </Text>
+              )}
+            </Flex>
+          </AlignCenterFlex>
+
+          <AlignCenterFlex w="100%">
+            {/* TODO: 비상상황관리 모달 열기 */}
+            <Flex w="100%" gap="8px" alignItems="center">
+              <EmergencyIcon w="24px" h="24px" cursor="pointer" />
+              {isOpenNavbar && (
+                <Text textStyle="TitleSmall" color="black">
+                  비상상황 관리
+                </Text>
+              )}
+            </Flex>
+          </AlignCenterFlex>
+
+          <AlignCenterFlex w="100%">
+            {/* TODO: 작업자 계정 추가 모달 열기 */}
+            <Flex w="100%" gap="8px" alignItems="center">
+              <AccountIcon w="24px" h="24px" cursor="pointer" />
+              {isOpenNavbar && (
+                <Text textStyle="TitleSmall" color="black">
+                  작업자 계정 추가
+                </Text>
+              )}
+            </Flex>
+          </AlignCenterFlex>
+        </Flex>
+      </Flex>
+      <AlignCenterFlex w="100%">
+        <Flex w="100%" gap="8px" alignItems="center">
+          <LogoutIcon w="24px" h="24px" cursor="pointer" />
+          {isOpenNavbar && (
+            <Text textStyle="TitleSmall" color="black">
+              로그아웃
+            </Text>
+          )}
+        </Flex>
+      </AlignCenterFlex>
+    </Flex>
+  );
+}
+
+export default HomeNavigationBar;
