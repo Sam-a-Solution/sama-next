53a546fc4938f162f326958904d9d5bb5fb95eaa
dingPie <cpie1216@gmail.com>
2023-05-09 11:22:33 +0900

    feat(Text.ts): 🎸Text default

---
+++ src/styles/theme/components/single/Text.ts
@@ -0,0 +1,14 @@
+import { ComponentSingleStyleConfig } from '@chakra-ui/react';
+
+export const Text: ComponentSingleStyleConfig = {
+  baseStyle: {
+    // color: 'black',
+    _dark: { color: 'black' },
+    // fontWeight: 'regular',
+    textStyle: 'md',
+    fontFamily: 'pretendard',
+  },
+  defaultProps: {},
+  sizes: {},
+  variants: {},
+};
