d6059fdd0977fcf04d2e1c7449df23c10c9ca6c2
dingPie <cpie1216@gmail.com>
2023-05-10 10:33:03 +0900

    feat(ResetPasswordResultPage): 🎸비밀번호 초기화 결과 페이지 UI 구현

--- src/pages/login/resetPassword/result.tsx
+++ src/pages/login/resetPassword/result.tsx
@@ -2,8 +2,10 @@ import Head from 'next/head';
 
 import FindIdPage from '@components/FindIdPage/FindIdPage';
 import FindIdResultPage from '@components/FindIdResultPage/FindIdResultPage';
+import ResetPasswordResultPage from '@components/ResetPasswordResultPage';
 import LoginHeader from '@components/common/@Layout/Header/LoginHeader';
 import HomeLayout from '@components/common/@Layout/HomeLayout';
+import LoginLayout from '@components/common/@Layout/LoginLayout';
 
 import withUnAuthGuard from '@hocs/withUnAuthGuard';
 
@@ -13,7 +15,8 @@ function ResetPasswordResult() {
       <Head>
         <title>똑똑한개발자 | 아이디/비밀번호 찾기</title>
       </Head>
-      <HomeLayout header={<LoginHeader />} content={<FindIdResultPage />} />
+      {/* <HomeLayout header={<LoginHeader />} content={<FindIdResultPage />} /> */}
+      <LoginLayout content={<ResetPasswordResultPage />} />
     </>
   );
 }
