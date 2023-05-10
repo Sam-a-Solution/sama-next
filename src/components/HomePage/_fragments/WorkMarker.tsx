00edd291136de7e102254ed370d67a4421467307
dingPie <cpie1216@gmail.com>
2023-05-10 14:26:33 +0900

    feat(WorkMarker.tsx): 🎸 workmarker asset, 컴포넌트 추가

---
+++ src/components/HomePage/_fragments/WorkMarker.tsx
@@ -0,0 +1,54 @@
+import React, { memo } from 'react';
+
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+import { WorkType } from 'generated/apis/@types/data-contracts';
+import {
+  MarkerEmergencyIcon,
+  MarkerStopIcon,
+  MarkerWorkIcon,
+} from 'generated/icons/MyIcons';
+
+interface WorkMarkerProps {
+  work: WorkType;
+}
+
+const WorkMarker = ({ work }: WorkMarkerProps) => {
+  return (
+    <Flex flexDir="column" gap="4px" alignItems="center">
+      {work.status === 'EMERGENCY' && <MarkerEmergencyIcon w="34px" h="34px" />}
+      {work.status === 'PROGRESS' && <MarkerWorkIcon w="34px" h="34px" />}
+      {(work.status === 'END' || work.status === 'READY') && (
+        <MarkerStopIcon w="34px" h="34px" />
+      )}
+      <Flex flexDir="column">
+        <Box
+          alignSelf="center"
+          borderRightColor="transparent"
+          borderRightWidth="8px"
+          borderLeftColor="transparent"
+          borderLeftWidth="8px"
+          borderBottomColor="rgba(0, 0, 0, 0.66)"
+          borderBottomWidth="10px"
+        />
+        <Flex
+          flexDir="column"
+          bgColor="rgba(0, 0, 0, 0.66)"
+          borderRadius="4px"
+          alignItems="center"
+          py="4px"
+          px="8px"
+        >
+          <Text color="white" size="sm" fontWeight="700">
+            {work.heavyEquipmentType?.koreaName}
+          </Text>
+          <Text color="white" size="sm">
+            {work.user}
+          </Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default WorkMarker;
