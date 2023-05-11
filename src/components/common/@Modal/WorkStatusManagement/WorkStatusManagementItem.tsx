a1f5866374ee61f6454452e845578d1be0dec223
dingPie <cpie1216@gmail.com>
2023-05-11 12:34:41 +0900

    feat(WorkStatusManagementItem.tsx): 🎸워크 아이템 분리, 상태처리값 변경

---
+++ src/components/common/@Modal/WorkStatusManagement/WorkStatusManagementItem.tsx
@@ -0,0 +1,119 @@
+import { memo } from 'react';
+
+import dayjs from 'dayjs';
+
+import { Button, Flex, Text, Tr } from '@chakra-ui/react';
+
+import StatusBadge from '@components/common/@Badge/StatusBadge';
+import CustomTd from '@components/common/@Table/CustomTd';
+
+import { WorkLogType } from 'generated/apis/@types/data-contracts';
+
+interface WorkStatusManagementItemProps {
+  item: WorkLogType;
+  index: number;
+  page: number;
+  onClickOpenReportModal: ({
+    item,
+    isEditable,
+  }: {
+    item: WorkLogType;
+    isEditable: boolean;
+  }) => void;
+  onClickManagerCheck: ({ item }: { item: WorkLogType }) => void;
+}
+
+const WorkStatusManagementItem = ({
+  item,
+  index,
+  page,
+  onClickManagerCheck,
+  onClickOpenReportModal,
+}: WorkStatusManagementItemProps) => {
+  const isEditable =
+    !item?.isChecked &&
+    (item?.statusDisplay === '대기' || item?.statusDisplay === '종료');
+
+  const isCanCheck =
+    item?.statusDisplay === '대기' || item?.statusDisplay === '종료';
+
+  console.log('dddd', item?.statusDisplay);
+
+  return (
+    <Tr key={`${item.user}-${item.id}-${index}`} h="60px">
+      <CustomTd w="100px">
+        <Text textStyle="Text" color="black">
+          {page * 10 - 10 + index + 1}
+        </Text>
+      </CustomTd>
+      <CustomTd w="160px">
+        <Text textStyle="Text" color="black">
+          {item?.user || ''}
+        </Text>
+      </CustomTd>
+      <CustomTd w="160px">
+        <Text textStyle="Text" color="black">
+          {item?.heavyEquipmentType?.koreaName || ''}
+        </Text>
+      </CustomTd>
+      <CustomTd w="80px">
+        <StatusBadge status={item?.statusDisplay} />
+      </CustomTd>
+      <CustomTd w="140px">
+        {/* 관리자 확인이 완료되었거나, 작업 미종료시 작업내용 수정 불가 */}
+        <Button
+          w="68px"
+          h="30px"
+          variant="outline"
+          colorScheme="primary"
+          onClick={() => onClickOpenReportModal({ item, isEditable })}
+        >
+          작업 내용
+        </Button>
+      </CustomTd>
+      <CustomTd w="220px">
+        <Text textStyle="Text" color="black">
+          {dayjs(item.startTime).format('HH:mm:ss') || '-'}
+        </Text>
+      </CustomTd>
+      <CustomTd w="220px">
+        <Text textStyle="Text" color="black">
+          {item?.endTime ? dayjs(item?.endTime).format('HH:mm:ss') : '-'}
+        </Text>
+      </CustomTd>
+      <CustomTd w="200px">
+        {item?.isChecked ? (
+          <Flex flexDir="column">
+            <Text color="black" textAlign="center">
+              {item?.checkManager || ''}
+            </Text>
+            <Text color="black" textAlign="center">
+              {dayjs(item?.checkManagerTime).format('YY-MM-DD HH:mm:ss')}
+            </Text>
+          </Flex>
+        ) : (
+          <Button
+            w="80px"
+            h="30px"
+            bg={'primary.500'}
+            border={'1px solid'}
+            isDisabled={!isCanCheck}
+            _disabled={{
+              bg: 'gray.400',
+              border: 'none',
+              _hover: {
+                bg: 'gray.400',
+              },
+            }}
+            color="white"
+            onClick={() => onClickManagerCheck({ item })}
+          >
+            확인
+          </Button>
+        )}
+      </CustomTd>
+    </Tr>
+  );
+};
+
+export default memo(WorkStatusManagementItem);
